b2d6e4aabc6f43494d3d051e2ef55f0993f1aebb
distated <ilyamed06072008@gmail.com>
2024-11-01 16:11:16 +0300

    Add files via upload

---
+++ config.yaml.py
@@ -0,0 +1,6 @@
+# config.yaml
+path: C:\Users\user\PycharmProjects\pythonProject2\dataset
+train: C:\Users\user\PycharmProjects\pythonProject2\dataset\images
+val:C:\Users\user\PycharmProjects\pythonProject2\dataset\images
+nc: 2
+names:  ['roki-2', 'walker']
